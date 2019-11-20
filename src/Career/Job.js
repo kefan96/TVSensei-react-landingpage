3070fd4f100c88559b4c7b4cb300204c0829e9fe
Kefan Yang <yang.3949@buckeyemail.osu.edu>
2019-11-20 14:51:37 -0500

    career page

--- src/Career/Job.js
+++ src/Career/Job.js
@@ -3,9 +3,20 @@ import './Job.css';
 
 class Job extends Component {
     render() {
+        const {title} = this.props;
+        let icon="";
+        if (title == "Web Development") {
+            icon = "fas fa-code job-icon";
+        } else if (title == "ELL Lesson Development") {
+            icon = "fas fa-glasses job-icon";
+        } else if (title == "Outreach Management") {
+            icon = "fas fa-search-dollar job-icon";
+        }
+
+
         return(
             <div className="job">
-                <i class="fas fa-code job-icon"></i><span className="job-title">Web Developer</span>
+                <i class={icon}></i><span className="job-title">{title}</span>
             </div>
         );
     }
--- src/Career/JobList.js
+++ src/Career/JobList.js
@@ -6,8 +6,20 @@ class JobList extends Component {
     render() {
         return(
             <section className="job-list">
-                <div className="row">
-                <Job className="col-md-4"/>
+                <div className="job-container">
+                    <div className="job-card">
+                        <Job title="Web Development" />
+                    </div>
+                    <div className="job-card">
+                        <Job title="ELL Lesson Development" />
+                    </div>
+                    <div className="job-card">
+                        <Job title="Outreach Management" />
+                    </div>
+                </div>
+                <div className="job-list-text text-center">
+                    <h2>Interested?</h2>
+                    <p>Find more information <a href="https://docs.google.com/document/d/1Nh-1ynwkJq7XSE37Vn6wNAk1AV1PbhOD4bkIlsd7pJY/edit">here</a> <br />Or send your resume to <email>henry@tvsensei.com</email></p>
                 </div>
             </section>
         );
